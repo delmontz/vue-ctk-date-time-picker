f1b2d000f872b9226688583ea9ec8b2c8d5aebb1
Takanuma Kouyu <k_takanuma@yumemi.co.jp>
2019-11-22 14:50:54 +0900

    fix: タイポ修正

--- src/VueCtkDateTimePicker/index.vue
+++ src/VueCtkDateTimePicker/index.vue
@@ -88,8 +88,8 @@
 
   const updateMomentLocale = (locale, firstDayOfWeek) => {
     if(locale === 'ja'){
-      moment.locale("ja", {
-        months: '1月_2月_3月_4月_5月_6月_7月_8月_9月_10月_11月_12月'.split()
+      moment.locale('ja', {
+        months: '1月_2月_3月_4月_5月_6月_7月_8月_9月_10月_11月_12月'.split('_')
       });
     }else{
       moment.locale(locale)
